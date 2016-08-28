fa6128788844635345c98d7b79e901549ccaf25e
elzup <guild0105@gmail.com>
2016-08-29 05:27:53 +0900

    Add active users emit

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
 const io = require('socket.io')(8080)
 
 io.sockets.on('connection', (socket) => {
-	console.log('connected')
+	console.log('new connection: ' + socket.id)
+	const _id = {id: socket.id}
+	socket.broadcast.emit('new', _id)
+
+	socket.on('new:sub', (data) => {
+		Object.assign(data, _id)
+		console.log(data)
+		socket.broadcast.emit('new:sub', data)
+	})
 
 	socket.on('draw', (data) => {
 		console.log(data)
@@ -19,4 +27,9 @@ io.sockets.on('connection', (socket) => {
 		console.log(width)
 		socket.broadcast.emit('lineWidth', width)
 	})
+
+	socket.on('disconnect', () => {
+		console.log('disconnect: ' + _id)
+		socket.broadcast.emit('remove', _id)
+	});
 })
