bbd104c76afdec799e28e0d45c24bd18420c7b99
elzup <guild0105@gmail.com>
2016-08-29 23:48:18 +0900

    Use eslint-config-xo

--- index.js
+++ index.js
@@ -2,28 +2,28 @@
 
 const io = require('socket.io')(8080)
 
-io.sockets.on('connection', (socket) => {
+io.sockets.on('connection', socket => {
 	console.log('new connection: ' + socket.id)
 	const _id = {id: socket.id}
 	socket.broadcast.emit('new', _id)
 
-	socket.on('new:sub', (data) => {
+	socket.on('new:sub', data => {
 		Object.assign(data, _id)
 		console.log(data)
 		socket.broadcast.emit('new:sub', data)
 	})
 
-	socket.on('draw', (data) => {
+	socket.on('draw', data => {
 		console.log(data)
 		socket.broadcast.emit('draw', data)
 	})
 
-	socket.on('color', (color) => {
+	socket.on('color', color => {
 		console.log(color)
 		socket.broadcast.emit('color', color)
 	})
 
-	socket.on('lineWidth', (width) => {
+	socket.on('lineWidth', width => {
 		console.log(width)
 		socket.broadcast.emit('lineWidth', width)
 	})
--- test.js
+++ test.js
@@ -1,6 +1,5 @@
-import test from 'ava';
-import fn from './';
+import test from 'ava'
 
 test('title', t => {
-	t.is(true, true);
-});
+	t.is(true, true)
+})
